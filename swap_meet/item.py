ac66301d74cd2a99bc4cac777961c26a66b24d14
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-21 14:46:07 -0700

    implement wave-2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
+import uuid
+
 class Item:
-    pass
+
+    def __init__(self,id= None):
+        if not id :
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+    def get_category(self):
+        return self.__class__.__name__
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,9 @@ class Vendor:
                 self.inventory.remove(i)
                 return i
         return None
+
+    def get_by_id(self,item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
