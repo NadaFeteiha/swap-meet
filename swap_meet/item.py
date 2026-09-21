0aa7ba4ef424c550d07b22b4557e37dd7ec35293
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-21 15:07:02 -0700

    implement wave-3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,21 @@
 import uuid
 
+
+
+# - Instances of `Vendor` have an instance method named `swap_items`
+#   - It takes 3 arguments:
+#   - `swap_items` takes 3 arguments:
+#     1. an instance of another `Vendor` (`other_vendor`), representing the friend that the vendor is swapping with
+#     2. an instance of an `Item` (`my_item`), representing the item this `Vendor` instance plans to give
+#     3. an instance of an `Item` (`their_item`), representing the item the friend `Vendor` plans to give
+
+#   - The method removes `my_item` from this `Vendor`'s inventory, and adds it to the friend's inventory
+#   - The method removes `their_item` from the other `Vendor`'s inventory, and adds it to this `Vendor`'s inventory
+#   - The method returns `True`
+#   - If this `Vendor`'s inventory doesn't contain `my_item` or the friend's inventory doesn't contain `their_item`, the method returns `False`
+
+
+
 class Item:
 
     def __init__(self,id= None):
@@ -10,4 +26,6 @@ class Item:
 
     def get_category(self):
         return self.__class__.__name__
-    
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,3 @@
-
 class Vendor:
 
     def __init__(self,inventory=None):
@@ -20,3 +19,23 @@ class Vendor:
             if item.id == item_id:
                 return item
         return None
+
+    def swap_items(self,other_vendor,my_item,their_item):
+        # if not my_item and not their_item:
+        #     return False
+
+        if my_item not in self.inventory:
+            return False
+
+        if their_item not in other_vendor.inventory:
+            return False
+        
+        self.remove(my_item)
+        self.add(their_item)
+
+        other_vendor.remove(their_item)
+        other_vendor.add(my_item)
+
+        return True
+        
+        
