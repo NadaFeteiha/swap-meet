97abfbafcc3e2ffec0f11f23bef3c9caede5a826
Daihuizi Zhu <zhudaihuizi@gmail.com>
2026-09-22 13:37:43 -0700

    Complete Wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -18,14 +18,34 @@ import uuid
 
 class Item:
 
-    def __init__(self,id= None):
+    def __init__(self,id= None, condition=0):
         if not id :
             self.id = uuid.uuid4().int
         else:
             self.id = id
+
+        # Initialize the condition of the item
+        self.condition = condition
+
 
     def get_category(self):
         return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Unusable"
+        elif self.condition == 1:
+            return "Poor"
+        elif self.condition == 2:
+            return "Fair"
+        elif self.condition == 3:
+            return "Good"
+        elif self.condition == 4:
+            return "Very Good"
+        elif self.condition == 5:
+            return "Excellent"
+
+    
