f09b23c760181483913babd7b8081fdbb66e27ce
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-23 18:54:50 -0700

    fix CONDITION_DESCRIPTIONS dictionary and str method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,32 +1,24 @@
 import uuid
 
 class Item:
+    CONDITION_DESCRIPTIONS = {
+        0: "Unusable",
+        1: "Poor",
+        2: "Fair",
+        3: "Good",
+        4: "Very Good",
+        5: "Excellent"
+    }
 
-    def __init__(self,id= None, condition=0.0):
-        self.CONDITION_DESCRIPTIONS = {
-                0.0: "Unusable", 
-                1.0: "Poor",  
-                2.0: "Fair",
-                3.0: "Good",
-                4.0: "Very Good",
-                5.0: "Excellent"
-            }
-        
-        if not id :
-            self.id = uuid.uuid4().int
-        else:
-            self.id = id
-
+    def __init__(self, id=None, condition=0.0):
+        self.id = uuid.uuid4().int if id is None else id
         self.condition = condition
-
 
     def get_category(self):
         return self.__class__.__name__
 
     def __str__(self):
-        return f"An object of type Item with id {self.id}."
+        return f"An object of type {self.get_category()} with id {self.id}."
 
     def condition_description(self):
-        return self.CONDITION_DESCRIPTIONS.get(self.condition, "Unknown")
-
-    
+        return self.CONDITION_DESCRIPTIONS.get(int(self.condition), "Unknown")
