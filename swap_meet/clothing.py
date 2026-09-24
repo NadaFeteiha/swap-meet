55a16d5daa9654b4dfc459729c3e43bbdf015732
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-23 18:58:41 -0700

    refactor the clothing, decor, and electronics to use the str in their parent to get benefit of inheritance.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,10 @@
 from swap_meet.item import Item
 
+
 class Clothing(Item):
     def __init__(self, id=None, condition=0.0, fabric="Unknown"):
         super().__init__(id, condition)
         self.fabric = fabric
+
     def __str__(self):
-        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        return f"{super().__str__()} It is made from {self.fabric} fabric."
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,11 +1,12 @@
 from swap_meet.item import Item
+
+
 class Decor(Item):
-    def __init__(self, id=None, condition=0, width=0, length=0):
+    def __init__(self, id=None, condition=0.0, width=0, length=0):
         super().__init__(id, condition)
         self.width = width
         self.length = length
 
     def __str__(self):
-        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
-    
-    
+        return (f"{super().__str__()} It takes up a {self.width} by "
+                f"{self.length} sized space.")
