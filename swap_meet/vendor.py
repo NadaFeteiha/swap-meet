bde54dec88619ec441fbef47482cd1cd104006c0
NadaFeteiha <nada.feteiha@gmail.com>
2026-09-23 18:48:25 -0700

    refactor vendor to enhane the remove function and swap_first_item

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,36 +1,31 @@
-
 class Vendor:
 
-    def __init__(self,inventory=None):
-        self.inventory = inventory or []
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
 
-    def add(self,item):
+    def add(self, item):
         self.inventory.append(item)
-        return self.inventory[-1]
+        return item
 
-    def remove(self,item):
-        for i in self.inventory:
-            if i == item:
-                self.inventory.remove(i)
-                return i
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
         return None
 
-    def get_by_id(self,item_id):
+    def get_by_id(self, item_id):
         for item in self.inventory:
             if item.id == item_id:
                 return item
         return None
 
-    def swap_items(self,other_vendor,my_item,their_item):
-        if not my_item and not their_item:
-            return False
-
+    def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory:
             return False
 
         if their_item not in other_vendor.inventory:
             return False
-        
+
         self.remove(my_item)
         self.add(their_item)
 
@@ -39,34 +34,29 @@ class Vendor:
 
         return True
 
-    def swap_first_item(self,other_vendor):
+    def swap_first_item(self, other_vendor):
         if not self.inventory or not other_vendor.inventory:
             return False
-        my_item = self.inventory[0]
-        their_item= other_vendor.inventory[0]
 
-        return self.swap_items(other_vendor,my_item,their_item)
+        return self.swap_items(
+            other_vendor, self.inventory[0], other_vendor.inventory[0])
 
-        
-    def get_by_category(self,category):
-        if category is None or category == "" or category == "Unknown":
-            return []
-        else:
-            return [item for item in self.inventory if item.get_category() == category]
+    def get_by_category(self, category):
+        return [item for item in self.inventory
+                if item.get_category() == category]
 
-
-    def get_best_by_category(self,category):
+    def get_best_by_category(self, category):
         items = self.get_by_category(category)
         if not items:
             return None
 
         return max(items, key=lambda item: item.condition)
 
-    def swap_best_by_category(self,other_vendor,my_priority,their_priority):
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
         my_item = self.get_best_by_category(their_priority)
         their_item = other_vendor.get_best_by_category(my_priority)
 
         if not my_item or not their_item:
             return False
 
-        return self.swap_items(other_vendor,my_item,their_item)
+        return self.swap_items(other_vendor, my_item, their_item)
